06b8245779620b6ae7c1a33b9495297c47344417
DOCENTE <DOCENTE@ULELAB341>
2023-08-25 17:28:13 -0500

    Agregar archivos de proyecto.

---
+++ Ejemplo04/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ejemplo04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un numero: ");
+            string numeroTexto = Console.ReadLine();
+            int numero = int.Parse(numeroTexto);
+            bool esPar = numero % 2 == 0;
+            if (esPar)
+            {
+                Console.WriteLine("Es par");
+            }
+            else {
+                Console.WriteLine("Es impar");
+            }
+        }
+    }
+}
